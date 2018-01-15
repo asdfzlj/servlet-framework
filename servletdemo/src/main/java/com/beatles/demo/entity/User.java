8e3fd180c4e7ad5c657e82c4e395b18bf842a220
beatles213 <23313963@qq.com>
2018-01-15 15:20:50 +0800

    initial project framework

---
+++ servletdemo/src/main/java/com/beatles/demo/entity/User.java
@@ -0,0 +1,9 @@
+package com.beatles.demo.entity;
+/**
+ * 用户实体
+ * @author zhang lj
+ * @createTime 2018年1月15日 下午3:05:24
+ */
+public class User {
+	
+}
---
+++ servletdemo/src/test/java/com/beatles/Test.java
@@ -0,0 +1,12 @@
+package com.beatles;
+
+/**
+ * @author zhang lj
+ * @createTime 2018年1月15日 下午3:06:12
+ */
+public class Test {
+	@org.junit.Test
+	public void test() {
+		
+	}
+}
